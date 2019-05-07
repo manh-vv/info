bb017ae6a29dab3b12dd5e1923ff28dae7025c61
manhvu <manh.vv.htth@gmail.com>
2019-05-08 06:05:31 +0700

    Updates

---
+++ precache-manifest.0971c0084578b51f023a2bb58f024976.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "95fef60db00fbb1dec63",
+    "url": "/js/chunk-vendors.47350325.js"
+  },
+  {
+    "revision": "4f9c1b5bdb9ae4b3d4ee",
+    "url": "/js/app.c5029e30.js"
+  },
+  {
+    "revision": "7c3e2fc3dc7e81865e69",
+    "url": "/js/about.4e3890bd.js"
+  },
+  {
+    "revision": "aebadf3cbdff4945652883f95153550c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "311f3d62e4d756762117f09561c04706",
+    "url": "/img/IMG_0515.311f3d62.png"
+  },
+  {
+    "revision": "4f9c1b5bdb9ae4b3d4ee",
+    "url": "/css/app.2c1d0c0e.css"
+  }
+];
