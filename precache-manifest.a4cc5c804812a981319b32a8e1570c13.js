7eee715ca2bc2953d3ebdd512c2d663a60d5a8db
manhvu <manhvu@orgos.net>
2019-12-09 13:51:25 +0700

    Updates

---
+++ precache-manifest.a4cc5c804812a981319b32a8e1570c13.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de33e3a88e24dd42dcb1",
+    "url": "/info/css/app.3512e37f.css"
+  },
+  {
+    "revision": "311f3d62e4d756762117f09561c04706",
+    "url": "/info/img/IMG_0515.311f3d62.png"
+  },
+  {
+    "revision": "07e1f4ac5d43b48e3ea7165d93d05f9d",
+    "url": "/info/index.html"
+  },
+  {
+    "revision": "a8b02dec53e7270cde41",
+    "url": "/info/js/about.43a42067.js"
+  },
+  {
+    "revision": "de33e3a88e24dd42dcb1",
+    "url": "/info/js/app.cabdf977.js"
+  },
+  {
+    "revision": "55710b2e8c790d73d8b2",
+    "url": "/info/js/chunk-vendors.1e49b93b.js"
+  },
+  {
+    "revision": "0268d1b56ac157b0d87790392e8f93f0",
+    "url": "/info/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/info/robots.txt"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/info/precache-manifest.f681bfddc5027ce8e3b86d7172d505b7.js"
+  "/info/precache-manifest.a4cc5c804812a981319b32a8e1570c13.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "info1"});
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,5 +31,4 @@ workbox.core.setCacheNameDetails({prefix: "info1"});
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
